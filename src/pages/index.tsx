850393e4cc13b795f89757aa24cab76f84a1e784
jefino9488 <jefinojacob9488@gmail.com>
2025-01-17 10:48:35 +0530

    update icon, dependency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
+        <link rel="icon" href={siteConfig.icon} />
         <title>{siteConfig.title}</title>
       </Head>
 
